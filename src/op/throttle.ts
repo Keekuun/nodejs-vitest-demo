1395c1fb92f88477e923b824a2106c7e0f0ba241
jeek <zkkysqs@gmail.com>
2024-05-13 11:32:53 +0800

    feat: update throttle doc

--- src/op/throttle.ts
+++ src/op/throttle.ts
@@ -26,7 +26,7 @@ function throttle2(fn: Function, ms = 200) {
   }
 }
 
-// 防抖版
+// leading: 延迟之前执行, trailing: 延迟之后执行
 function throttle(fn: Function, ms = 200, options = {leading: true, trailing: true}) {
   let timer: NodeJS.Timeout | null = null;
   let prev = 0;
@@ -34,6 +34,7 @@ function throttle(fn: Function, ms = 200, options = {leading: true, trailing: tr
   return function (...args: any[]) {
     const now = Date.now();
 
+    // 核心逻辑 1
     if (options.leading && !timer && now - prev >= ms) {
       fn.apply(this, args);
       prev = now;
@@ -41,6 +42,7 @@ function throttle(fn: Function, ms = 200, options = {leading: true, trailing: tr
 
     if (!timer) {
       timer = setTimeout(() => {
+        // 核心逻辑 2
         if (options.trailing && now - prev >= ms) {
           fn.apply(this, args);
         }
