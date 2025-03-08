3cb24ad9a5ff444a1abb5cb0780624727fe90ba6
Jeek <zkkysqs@gmail.com>
2025-03-08 21:03:02 +0800

    chore: update tsconfig

--- src/nestjs/main.ts
+++ src/nestjs/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from "@nestjs/core"
+import {AppModule} from "./app.module";
 
-import { AppModule } from "./app.module"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(3001)
     .then(() => {
-      console.log("Server is running on port 3000")
+      console.log("Server is running on port 3001")
     })
     .catch((err: any) => {
       console.error("Error starting server:", err)
